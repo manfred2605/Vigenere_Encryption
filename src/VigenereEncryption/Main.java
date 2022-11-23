3bcd03490ad24694d9d68bd57f3ee9979cebe95a
manfred2605 <80372230+manfred2605@users.noreply.github.com>
2022-11-23 17:39:16 -0600

    correccion de sintaxis 7

--- src/VigenereEncryption/Main.java
+++ src/VigenereEncryption/Main.java
@@ -7,38 +7,19 @@ public class Main extends Vigenere{
     public static void main(String[] args) {
 
         int choice;
+
+        String msg = null;
+        Vigenere vig = new Vigenere();
+
+        System.out.println("Digite la frase a encriptar: ");
         Scanner input = new Scanner(System.in);
-        Vigenere vig = new Vigenere("hola","ho");
+        System.out.println("Digite la clave: ");
+        Scanner input2 = new Scanner(System.in);
 
-        do {
-            System.out.println("""
-                    press 1 to Encrypt Message
-                    press 2 to Decrypt Message       
-                    press 0 for exit""");
+        msg = input.nextLine();
+        String password = input2.nextLine();
 
-            choice = input.nextInt();
-            switch (choice) {
-                case 1 -> {
-                    System.out.println("""
-                        press 1 to enter message
-                        press 2 to enter password  
-                        press 0 for exit""");
-                }
-
-                default -> System.out.println("Enter");
-            }
-        } while (choice != 0);
-
-
-
-
-
-
-
-
-
-
-
+        System.out.println();
 
 
     }
